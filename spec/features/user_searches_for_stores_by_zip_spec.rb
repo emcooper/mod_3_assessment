d60f7221e4a3f15bb28da1a76b73e74916e18c87
Ellen Cooper <ellencooper74@gmail.com>
2017-09-13 10:33:41 -0600

    adds feature test for store search user story

---
+++ spec/features/user_searches_for_stores_by_zip_spec.rb
@@ -0,0 +1,21 @@
+RSpec.feature "User searches for stores" do
+  scenario "they see number of total stores and store details" do
+    visit '/'
+    fill_in "search_bar", with: "80202"
+    click_on "Search"
+    expect(current_path).to eq("/search")
+
+    expect(page).to have_content("17 Total Stores")
+
+    within(".store-1") do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".city")
+      expect(page).to have_css(".distance")
+      expect(page).to have_css(".phone_number")
+      expect(page).to have_css(".store_type")
+    end
+
+    num_stores = find(".store").count
+    expect(num_stores).to eq(17)
+  end
+end
