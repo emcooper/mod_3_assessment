a9de1df312b72e280b20bd5864326754b2803dd6
Ellen Cooper <ellencooper74@gmail.com>
2017-09-13 09:42:20 -0600

    adds route and controller action for item show endpoint

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -19,4 +19,20 @@ describe "Items API" do
     expect(response_items.third["created_at"]).to be_nil
     expect(response_items.third["updated_at"]).to be_nil
   end
+
+  it "returns one item" do
+    items = create_list(:item, 3)
+
+    get "/api/v1/items/1"
+
+    expect(response).to be_success
+    response_items = JSON.parse(response.body)["item"]
+
+    expect(response_items["id"]).to eq(items.first.id)
+    expect(response_items["name"]).to eq(items.first.name)
+    expect(response_items["description"]).to eq(items.first.description)
+    expect(response_items["image_url"]).to eq(items.first.image_url)
+    expect(response_items["created_at"]).to be_nil
+    expect(response_items["updated_at"]).to be_nil
+  end
 end
