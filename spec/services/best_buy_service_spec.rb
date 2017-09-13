a3e56f422f00314a0fb9b1305a5139f4ddf6646a
Ellen Cooper <ellencooper74@gmail.com>
2017-09-13 12:00:17 -0600

    adds assertions to service test

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -5,8 +5,12 @@ describe BestBuyService do
     it ".find_by_filter returns store data within 25 miles of zip" do
       VCR.use_cassette("services/best_buy/find_by_filter") do
         stores = BestBuyService.find_by_filter({zip: 80202})[:stores]
-        
         expect(stores.count).to eq(10)
+        expect(stores.first[:longName]).to be_a(String)
+        expect(stores.first[:city]).to be_a(String)
+        expect(stores.first[:distance]).to be_a(Float)
+        expect(stores.first[:phone]).to be_a(String)
+        expect(stores.first[:storeType]).to be_a(String)
       end
     end
   end
